be3f13b2d2f6134defad05e282dbc4575667aa41
Joel Chandra Paul <joelcpaul02@gmail.com>
2026-03-25 16:36:55 -0400

    Lab exercise 8 completed

---
+++ Lab08_angular/src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { RemoveSpacesPipe } from '../remove-spaces.pipe';
+import { InputFormatDirective } from '../input-format.directive';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    RemoveSpacesPipe,
+    InputFormatDirective,
+    HeroDetailComponent
+  ],
+  templateUrl: './heroes.html',
+  styleUrl: './heroes.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
